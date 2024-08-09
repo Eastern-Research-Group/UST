49c62f179a5e746d544038cb650a5376a6bec1d9
Renae Myers <renaemyers@gmail.com>
2024-08-09 12:24:54 -0700

    new dispenser tables

--- ust/python/util/save_ddl.py
+++ ust/python/util/save_ddl.py
@@ -1,13 +1,13 @@
+from datetime import date
+import ntpath
 import os
 from pathlib import Path
 import sys  
 ROOT_PATH = Path(__file__).parent.parent.parent
 sys.path.append(os.path.join(ROOT_PATH, ''))
-from datetime import date
-import ntpath
 
-from python.util.logger_factory import logger
 from python.util import utils, config
+from python.util.logger_factory import logger
 
 
 schema = 'public'
