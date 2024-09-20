00d13d7cfdc8447221138a754f8ca58bb432d679
Renae Myers <renaemyers@gmail.com>
2024-09-20 11:42:34 -0700

    example value mapping

--- ust/python/state_processing/org_mapping_xwalks_example.py
+++ ust/python/state_processing/org_mapping_xwalks_example.py
@@ -5,21 +5,22 @@ ROOT_PATH = Path(__file__).parent.parent.parent
 sys.path.append(os.path.join(ROOT_PATH, ''))
 
 from python.util import utils
-from python.util.dataset import Dataset 
+from python.util.dataset_example import Dataset 
 from python.util.logger_factory import logger
 
 
 ust_or_release = 'ust' 			# Valid values are 'ust' or 'release'
-control_id = 0                  # Enter an integer that is the ust_control_id or release_control_id
+control_id = 1                  # Enter an integer that is the ust_control_id or release_control_id
 
 
 def main(dataset):
+
 	conn = utils.connect_db()
 	cur = conn.cursor()
 
 	sql = f"""select epa_column_name, organization_table_name, organization_column_name,
 					database_lookup_table, database_lookup_column 
-			from public.v_{dataset.ust_or_release}_table_population 
+			from example.v_{dataset.ust_or_release}_table_population 
 			where {dataset.ust_or_release}_control_id = %s and database_lookup_table is not null
 			order by table_sort_order, column_sort_order"""
 	cur.execute(sql, (control_id, ))
@@ -41,7 +42,7 @@ def main(dataset):
 
 		sql = f"""create or replace view {view_name} as 
 				select a.organization_value, a.epa_value, b.{epa_column_name2}
-				from public.v_{dataset.ust_or_release}_element_mapping a left join {database_lookup_table} b on a.epa_value = b.{database_lookup_column}
+				from example.v_{dataset.ust_or_release}_element_mapping a left join {database_lookup_table} b on a.epa_value = b.{database_lookup_column}
 				where a.{dataset.ust_or_release}_control_id = %s and a.epa_column_name = %s"""
 		# print(sql)
 		try:
