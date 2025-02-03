a6f5a923437e66d2f6b322d9ac41fc1cf891c894
Clara Pitkins <clara.pitkins@erg.com>
2025-02-03 15:16:47 -0500

    Update

--- ust/python/state_processing/qa_check.py
+++ ust/python/state_processing/qa_check.py
@@ -17,7 +17,7 @@ from python.util.logger_factory import logger
 
 
 ust_or_release = 'ust' 			# Valid values are 'ust' or 'release'
-control_id = 0              	# Enter an integer that is the ust_control_id or release_control_id
+control_id = 29              	# Enter an integer that is the ust_control_id or release_control_id
 
 # These variables can usually be left unset. This script will general an Excel spreadsheet in the appropriate state folder in the repo under /ust/python/exports/QAQC
 # This file directory and its contents are excluded from pushes to the repo by .gitignore.
