08489c64b3ff4a6c6bd9e34cbbd0ab4f09ee368d
Renae Myers <renae.myers@erg.com>
2025-04-03 14:08:11 -0400

    config update

--- ust/python/util/config.py
+++ ust/python/util/config.py
@@ -1,7 +1,7 @@
-db_user='ugst_ergone'
-db_password='Sunday1234'
-db_ip='4.36.57.13'
+db_user=None
+db_password=None
+db_ip=None
 db_connection_string = f'postgresql://{db_user}:{db_password}@{db_ip}:5432/'
-db_name='UGSTank'
+db_name=None
 
 local_ust_path = r'C:\Users\erguser\OneDrive - Eastern Research Group\Other Projects\UST\State Data\\'
