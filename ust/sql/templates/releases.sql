c80c339e762dcece91f1cadc93660e04f978307a
renaemyers <renaemyers@users.noreply.github.com>
2025-07-18 01:12:32 -0400

    Update releases.sql

--- ust/sql/templates/releases.sql
+++ ust/sql/templates/releases.sql
@@ -505,6 +505,7 @@ and table_name like '%_xwalk' order by 1;
 
 ust_or_release = 'release' 		 # Valid values are 'ust' or 'release' 
 control_id = ZZ                  # Enter an integer that is the release_control_id
+table_name = None                # Optional; enter the table name that contains the missing ID column. If None, the script will identify all tables that require an ID column.
 drop_existing = False 		     # Boolean, defaults to False. Set to True to drop the table if it exists before creating it new.
 write_sql = True                 # Boolean, defaults to True. If True, writes a SQL script recording the queries it ran to generate the tables.
 overwrite_sql_file = False       # Boolean, defaults to False. Set to True to overwrite an existing SQL file if it exists. This parameter has no effect if write_sql = False. 
