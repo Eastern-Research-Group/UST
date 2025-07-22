75cdded7b0cf320211ea76eae71520fe24c61cb0
renaemyers <renaemyers@users.noreply.github.com>
2025-07-21 23:52:43 -0400

    Update generate_value_mapping_sql.py

--- ust/python/state_processing/generate_value_mapping_sql.py
+++ ust/python/state_processing/generate_value_mapping_sql.py
@@ -15,7 +15,7 @@ control_id = 0                 	# Enter an integer that is the ust_control_id or
 only_incomplete = True  		# Boolean, defaults to True. Set to False to output mapping for all columns regardless if mapping was previously done. 
 overwrite_existing = True      	# Boolean, defaults to True. If True, will overwrite an existing file. Set to False to append to an existing file. 
 
-# These variables can usually be left unset. This script will general a SQL file in the appropriate state folder in the repo under /ust/sql/states
+# These variables can usually be left unset. This script will generate a SQL file in the appropriate state folder in the repo under /ust/sql/states
 export_file_path = None
 export_file_dir = None
 export_file_name = None
