24660c4c3e9f9f6067a28aac01481f6b8c724d46
Clara Pitkins <clara.pitkins@erg.com>
2026-05-14 12:14:26 -0400

    updates

--- ust/python/util/config.py
+++ ust/python/util/config.py
@@ -1,7 +1,11 @@
-db_user=None
-db_password=None
-db_ip=None
+db_user='ugst_ergone'
+db_password='Sunday1234'
+db_ip='4.36.57.13'
 db_connection_string = f'postgresql://{db_user}:{db_password}@{db_ip}:5432/'
-db_name=None
+db_name='UGSTank'
 
-local_ust_path = r'C:\Users\erguser\OneDrive - Eastern Research Group\Other Projects\UST\State Data\\'
+hazsub_email = 'John.Wilhelmi@erg.com'
+hazsub_cc = 'victoria.andrews@erg.com; renae.myers@erg.com'
+
+element_row_counts_email = 'victoria.andrews@erg.com'
+element_row_counts_cc = 'renae.myers@erg.com; adam.langmaid@erg.com'
